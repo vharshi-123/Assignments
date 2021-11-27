e7a9f31a7d89e8826a366817c3c710cec3cfb771
vharshi-123 <harshineevakulabaranam@gmail.com>
2021-11-27 07:41:05 +0530

    REST

---
+++ Spring REST/rest3/rest3question/src/main/java/rest3/rest3question/Rest3questionApplication.java
@@ -0,0 +1,13 @@
+package rest3.rest3question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest3questionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rest3questionApplication.class, args);
+	}
+
+}
---
+++ Spring REST/rest3/rest3question/src/test/java/rest3/rest3question/Rest3questionApplicationTests.java
@@ -0,0 +1,13 @@
+package rest3.rest3question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest3questionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
